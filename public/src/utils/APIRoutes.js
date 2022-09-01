fb0973c1aa3a077fc937c23b14726f6a6d09f80d
Meiramovaaa <zikosha13@gmail.com>
2022-09-01 13:42:21 +0600

    change

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,5 +1,5 @@
-export const host = "http://localhost:5000" 
-// export const host = "http://chatapi.ziyameiramova.site"
+// export const host = "http://localhost:5000" 
+export const host = "http://chatapi.ziyameiramova.site"
 export const registerRoute = `${host}/api/auth/register`
 export const loginRoute = `${host}/api/auth/login`
 export const allUsersRoute = `${host}/api/auth/allusers`;
